a0c58ecd4d5afeaf75786bd72e3356f9ab332464
VINAY MITTAL <vinay@vinsol.com>
2016-02-29 20:17:55 +0530

    subscribable functionality pass 2

--- app/assets/javascripts/spree/backend/spree_items_subscriptions.js
+++ app/assets/javascripts/spree/backend/spree_items_subscriptions.js
@@ -1,2 +1,11 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
+$(function() {
+  $("[data-hook='admin_product_form_subscribable']").find("#product_subscribable").on("change", function(){
+    if ($(this).checked == true) {
+      $("#subscribable_options").show();
+    } else {
+      $("#subscribable_options").hide();
+    }
+  });
+});
