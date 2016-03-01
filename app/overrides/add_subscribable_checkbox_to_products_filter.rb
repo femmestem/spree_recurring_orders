1f4c6bd07177c2466c9188d27ff1cfaa53e12257
VINAY MITTAL <vinay@vinsol.com>
2016-03-02 02:24:48 +0530

    naming changes and fixes

--- app/overrides/add_subscribable_checkbox_to_products_filter.rb
+++ app/overrides/add_subscribable_checkbox_to_products_filter.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/products/index',
   name: 'add_subscribable_filter_to_products',
   insert_bottom: "[data-hook='admin_products_index_search'] .col-md-12",
-  text: "spree/admin/products/_subscribable_filter"
+  partial: "spree/admin/products/subscribable_filter"
 )
