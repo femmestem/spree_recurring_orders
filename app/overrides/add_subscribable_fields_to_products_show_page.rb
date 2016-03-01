5629e3f98c9cfd06d7e84fca6f05b520d5551a88
VINAY MITTAL <vinay@vinsol.com>
2016-03-02 02:31:45 +0530

    CR changes applied to this branch also

--- app/overrides/add_subscribable_fields_to_products_show_page.rb
+++ app/overrides/add_subscribable_fields_to_products_show_page.rb
@@ -2,19 +2,5 @@ Deface::Override.new(
   virtual_path: "spree/products/_cart_form",
   name: "add_subscribable_fields_to_products_show",
   insert_after: ".input-group",
-  text: "<% if @product.subscribable? %>
-          <span class='input-group-btn'>
-            <%= button_tag class: 'btn btn-success', id: 'subscribe-button', type: :submit, name: :subscribe, value: '1' do %>
-              <%= Spree.t(:subscribe_now) %>
-            <% end %>
-          </span>
-          <% subscription = Spree::Subscription.new %>
-          <span>
-            <%= collection_select :subscription, :subscription_frequency_id, @product.subscription_frequencies, :id, :title %>
-          </span>
-          <br>
-          <span>
-            <%= date_select :subscription, :end_date, { default: Date.today }, {} %>
-          </span>
-        <% end %>"
+  partial: "spree/products/subscription_fields"
 )
