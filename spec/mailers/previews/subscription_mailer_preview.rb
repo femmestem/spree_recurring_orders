43ebf304e2edcf9e77fa930c3e61f7d7d26fbbc7
VINAY MITTAL <vinay@vinsol.com>
2016-03-03 20:33:39 +0530

    spec folder removed

--- spec/mailers/previews/subscription_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/subscription_mailer
-class SubscriptionMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/subscription_mailer/notify_user
-  def notify_user
-    SubscriptionMailer.notify_user
-  end
-
-end
