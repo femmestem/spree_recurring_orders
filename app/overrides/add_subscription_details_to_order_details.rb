756d430ad9dbef3761659735e8a07a607c68eb44
VINAY MITTAL <vinay@vinsol.com>
2016-03-08 19:36:45 +0530

    deface improved and made for order details page also

--- app/overrides/add_subscription_details_to_order_details.rb
+++ app/overrides/add_subscription_details_to_order_details.rb
@@ -1,20 +1,20 @@
 Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: "add_subscription_header_to_order_details",
-  insert_bottom: "#line_items thead tr",
+  insert_bottom: '[data-hook="order_details_line_items_headers"]',
   partial: "spree/orders/cart_subscription_header"
 )
 
 Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
-  name: "add_subscription_header_to_order_details",
-  insert_bottom: "#line_items tbody tr",
-  partial: "spree/orders/subscription_field"
+  name: "add_subscription_body_to_order_details",
+  insert_bottom: "[data-hook='order_details_line_item_row']",
+  partial: "spree/shared/subscription_field"
 )
 
 Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
-  name: "add_subscription_header_to_order_details",
-  insert_bottom: "#line_items tfoot tr",
+  name: "add_subscription_footer_to_order_details",
+  insert_bottom: ".total",
   partial: "spree/orders/cart_subscription_footer"
 )
