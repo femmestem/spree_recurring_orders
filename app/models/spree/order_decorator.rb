1c4c277690455b2e74fdf13c0c32957b7b44074a
VINAY MITTAL <vinay@vinsol.com>
2016-03-11 18:19:31 +0530

    validation fix

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Order.class_eval do
 
   has_one :order_subscription, class_name: "Spree::OrderSubscription", dependent: :destroy
-  has_one :parent_subscription, through: :order_subscription
+  has_one :parent_subscription, through: :order_subscription, source: :subscription
   has_many :subscriptions, class_name: "Spree::Subscription",
                            foreign_key: :parent_order_id,
                            dependent: :restrict_with_error
