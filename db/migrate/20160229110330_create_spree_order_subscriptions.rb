2f8816a33f7fd757425d94df1a9ce9ea813212c8
VINAY MITTAL <vinay@vinsol.com>
2016-03-15 12:06:53 +0530

    remove dependency and foreign keys

--- db/migrate/20160229110330_create_spree_order_subscriptions.rb
+++ db/migrate/20160229110330_create_spree_order_subscriptions.rb
@@ -1,8 +1,8 @@
 class CreateSpreeOrderSubscriptions < ActiveRecord::Migration
   def change
     create_table :spree_order_subscriptions do |t|
-      t.references :subscription, index: true, foreign_key: true
-      t.references :order, index: true, foreign_key: true
+      t.references :subscription, index: true
+      t.references :order, index: true
       t.date :failed_at
       t.text :failure_reasons
     end
--- db/migrate/20160229122857_create_spree_product_subscription_frequencies.rb
+++ db/migrate/20160229122857_create_spree_product_subscription_frequencies.rb
@@ -1,8 +1,8 @@
 class CreateSpreeProductSubscriptionFrequencies < ActiveRecord::Migration
   def change
     create_table :spree_product_subscription_frequencies do |t|
-      t.references :product, index: true, foreign_key: true
-      t.references :subscription_frequency, foreign_key: true
+      t.references :product, index: true
+      t.references :subscription_frequency
     end
   end
 end
