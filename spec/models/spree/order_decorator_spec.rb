0993f80a6ef96dc49e69cd4871bda984fb456df5
VINAY MITTAL <vinay@vinsol.com>
2016-03-16 16:06:00 +0530

    order method specs

--- spec/models/spree/order_decorator_spec.rb
+++ spec/models/spree/order_decorator_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Spree::Order do
 
+  let(:enabled_subscription) { create(:valid_subscription, enabled: true) }
+  let(:disabled_subscription) { create(:valid_subscription, enabled: false) }
+  let(:subscriptions) { [enabled_subscription, disabled_subscription] }
+  let(:order_with_subscriptions) { create(:order_with_line_items, subscriptions: subscriptions) }
+
   describe "associations" do
     it { expect(subject).to have_one(:order_subscription).class_name("Spree::OrderSubscription").dependent(:destroy) }
     it { expect(subject).to have_one(:parent_subscription).through(:order_subscription).source(:subscription) }
@@ -14,7 +19,22 @@ describe Spree::Order do
 
   describe "methods" do
     context "#available_payment_methods" do
+      let(:order_without_subscriptions) { create(:order_with_line_items) }
+      let(:credit_card_payment_method) { create(:credit_card_payment_method) }
+      let(:check_payment_method) { create(:check_payment_method) }
+      context "order with subscriptions" do
+        it { expect(order_with_subscriptions.available_payment_methods).to include credit_card_payment_method }
+        it { expect(order_with_subscriptions.available_payment_methods).to_not include check_payment_method }
+      end
 
+      context "order without subscriptions" do
+        it { expect(order_without_subscriptions.available_payment_methods).to include check_payment_method }
+        it { expect(order_without_subscriptions.available_payment_methods).to include credit_card_payment_method }
+      end
+    end
+
+    context "#any_disabled_subscription?" do
+      it { expect(order_with_subscriptions).to be_any_disabled_subscription  }
     end
   end
 
