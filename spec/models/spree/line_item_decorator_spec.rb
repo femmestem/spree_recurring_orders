fced165461a0ebfcd87a60df1868e6a13ffbc693
VINAY MITTAL <vinay@vinsol.com>
2016-03-17 12:00:09 +0530

    start line item spec

--- spec/models/spree/line_item_decorator_spec.rb
+++ spec/models/spree/line_item_decorator_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Spree::LineItem do
 
+  let(:order) { create(:completed_order_with_totals) }
+  let(:active_subscription) { create(:valid_subscription, enabled: true, parent_order: order) }
+  let(:line_item_without_subscription_attributes) { create(:line_item, order: order) }
+  let(:line_item_with_subscription_attributes) { create(:line_item, order: order, subscription_frequency_id: 1, delivery_number: 6) }
+
   describe "callbacks" do
     it { expect(subject).to callback(:create_subscription!).after(:create).if(:subscribable?) }
     it { expect(subject).to callback(:update_subscription_quantity).after(:update).if(:can_update_subscription_quantity?) }
@@ -16,6 +21,13 @@ describe Spree::LineItem do
     it { expect(subject).to respond_to :subscribe= }
     it { expect(subject).to respond_to :subscription_frequency_id }
     it { expect(subject).to respond_to :subscription_frequency_id= }
+  end
+
+  describe "methods" do
+    context "#subscription_attributes_present?" do
+      it { expect(line_item_without_subscription_attributes).to be_subscription_attributes_present }
+      it { expect(line_item_with_subscription_attributes).to_not be_subscription_attributes_present }
+    end
   end
 
 end
